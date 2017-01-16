3f11306b0f3c1df99ccd7d0afb6c8c3b4440cb03
mattRCB <mattsteinrcb@gmail.com>
2017-01-15 20:59:49 -0500

    fix index.php

--- index.php
+++ index.php
@@ -1,17 +1,1 @@
-	function drawGraph() {
-    // Create the chart
-	    Highcharts.chart('graph-well', {
-			chart: {
-				type: 'line',
-				zoomType: 'xy'
-        	},
-
-			title: {
-	            text: 'Highcharts data from Google Spreadsheets'
-	        },
-
-	        data: {
-	            googleSpreadsheetKey: '1swa7IJkys5J2UmZRswntYrvgo_lm7M9ADgW7W0mgC-o'
-	        }
-	    });
-	}; // drawGraph()
+<?php include_once("index.html"); ?>
